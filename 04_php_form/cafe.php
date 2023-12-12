486ef22265da2bc92074e5efeb6eda8b8c3b84cf
Reppi0520 <takayoshi520@gmail.com>
2023-12-13 05:37:19 +0900

    put file in

---
+++ 04_php_form/cafe.php
@@ -0,0 +1,327 @@
+<DOCTYPE html>
+<html lang = "ja" >
+<head>
+    <meta charset="UTF-8" >
+    <link rel="stylesheet" href="cafe.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script>"js/jquery.bgswitcher.js"</script>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+
+
+</head>
+
+<body>
+<div id = "full">
+    <div class = "info-covid">
+        <p class = "info-covid-link">新型コロナウイルスに対する取り組みの最新情報をご案内</p>
+
+    </div>
+   
+
+    <div class = "fv">
+        <div class = "fv-background" >
+        <header id = "js-header" class = "header">
+    <div class = "wrap">
+    <div class= "header-contents">
+        <img class = "header-logo" src="cafe/img/logo.png" alt="ヘッダーロゴ">
+        <nav class = "header-navi">
+            <ul class = "navi-list">
+                <li>はじめに</li>
+                <li>体験</li>
+                <li>お問い合わせ</li>
+            </ul>
+
+        </nav>
+        <div class = "signin">
+            <p>サインイン</p>
+
+        </div>
+
+    </div>    
+    </div>
+   
+
+
+        </header>
+        
+        
+            <p class = "catchcopy wrap">あなたの好きな空間を作る。</p>
+
+        </div>
+    </div>
+
+    <div class = "contents">
+        <div class = "box1">
+            <div class = "access wrap">
+                <div class = " mini-box ">
+                    <img class = mini-box-img src="cafe/img/cafe1.jpg" alt="ヘッダーロゴ">
+                    <ul class = "mini-box-contents">
+                        <li>東京</li>
+                        <li>車で15分</li>
+                    </ul>
+                </div>
+                <div class = " mini-box ">
+                    <img class = mini-box-img src="cafe/img/cafe2.jpg" alt="ヘッダーロゴ">
+                    <ul class = "mini-box-contents">
+                        <li>神奈川</li>
+                        <li>車で30分</li>
+                    </ul>
+                </div>
+                <div class = " mini-box ">
+                    <img class = mini-box-img src="cafe/img/cafe3.jpg" alt="ヘッダーロゴ">
+                    <ul class = "mini-box-contents">
+                        <li>愛知</li>
+                        <li>車で1時間</li>
+                    </ul>
+                </div>
+                <div class = " mini-box ">
+                    <img class = mini-box-img src="cafe/img/cafe4.jpg" alt="ヘッダーロゴ">
+                    <ul class = "mini-box-contents">
+                        <li>京都</li>
+                        <li>車で40分</li>
+                    </ul>
+                </div>
+                <div class = " mini-box ">
+                    <img class = mini-box-img src="cafe/img/cafe5.jpg" alt="ヘッダーロゴ">
+                    <ul class = "mini-box-contents">
+                        <li>岡山</li>
+                        <li>車で1.5時間</li>
+                    </ul>
+                </div>
+                <div class = " mini-box ">
+                    <img class = mini-box-img src="cafe/img/cafe6.jpg" alt="ヘッダーロゴ">
+                    <ul class = "mini-box-contents">
+                        <li>鹿児島</li>
+                        <li>車で50分</li>
+                    </ul>
+                </div>
+                <div class = " mini-box ">
+                    <img class = mini-box-img src="cafe/img/cafe7.jpg" alt="ヘッダーロゴ">
+                    <ul class = "mini-box-contents">
+                        <li>沖縄 </li>
+                        <li>車で2時間</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class = "location wrap">
+                <h2 class = "subject-tittle ">好きなロケーションを選ぼう</h2>
+                <div class = "locations">
+                    <div class = "location-box">
+                        <img class = location-image src="cafe/img/intro1.jpg" alt="ロケーション画像">
+                        <p class = lacation-tittle>クラシック</p>
+
+                    </div>
+                    <div class = "location-box">
+                        <img class = location-image src="cafe/img/intro2.jpg" alt="ロケーション画像">
+                        <p class = lacation-tittle>バー</p>
+
+                    </div>
+                    <div class = "location-box">
+                        <img class = location-image src="cafe/img/intro3.jpg" alt="ロケーション画像">
+                        <p class = lacation-tittle>キャンプ</p>
+
+                    </div>
+                    <div class = "location-box">
+                        <img class = location-image src="cafe/img/intro4.jpg" alt="ロケーション画像">
+                        <p class = lacation-tittle>リゾート</p>
+
+                    </div>
+                </div>
+
+
+            </div>
+            <div class = "wrap">
+            <div class = "gotoeat_campaign ">
+                <div class = "campaign">
+                    <p class = "campaign-tittle">
+                    Go To Eats
+                    </p>
+                    <ul class = "campaign-contents">
+                    <li>キャンペーンを利用して、全国で食事しよう。</li>
+                    <li>いつもと違う景色に囲まれてカラダもココロもリフレッシュ。</li>
+                    </ul>
+
+                </div>
+
+
+
+            </div>
+            </div>
+            
+        </div>
+        <div class = "box2">
+            <div class = "box2-contents wrap">
+            <h2 class = "subject-tittle ">カフェ作りを体験しよう</h2>
+
+            <p class = "subject-subtittle"> お店のエキスパートが案内するユニークな体験（直接対面型またはオンライン）。</p>
+           
+            <div class = "mini-boxies2">
+                <div class = "mini-box2 cafe-exp">
+                    <img class = "mini-box2-image" src="cafe/img/exp1.jpg" alt="カフェ作り体験画像1">
+                    <h3 class = "mini-box2-tittle" >ジョブ体験</h3>
+                    <p class = "mini-box2-subtittle">カフェカウンターを体験しよう。</p>
+
+                </div>
+                <div class = "mini-box2 cafe-exp">
+                    <img class = "mini-box2-image" src="cafe/img/exp2.jpg" alt="カフェ作り体験画像2">
+                    <h3 class = "mini-box2-tittle" >レシピ体験</h3>
+                    <p class = "mini-box2-subtittle">美味しいレシピを考えてみよう。</p>
+
+                </div>
+                <div class = "mini-box2 ">
+                    <img class = "mini-box2-image" src="cafe/img/exp3.jpg" alt="カフェ作り体験画像3">
+                    <h3 class = "mini-box2-tittle" >プロモーション体験</h3>
+                    <p class = "mini-box2-subtittle">お店の宣伝を手伝ってみよう。</p>
+
+                </div>
+            </div>
+
+            </div>
+
+
+        </div>
+        <div class = "box3 wrap">
+        <h2 class = "subject-tittle ">全国のホストに仲間入りしよう</h2>
+        <div class = "mini-boxies3">
+            <div class = "mini-box3 hosts">
+                <img class = "mini-box3-image" src="cafe/img/host1.jpg" alt="ホスト画像1">
+                <h3 class = "mini-box2-tittle" >ビジネス</h3>
+
+        </div>
+            <div class = "mini-box3 hosts">
+                <img class = "mini-box3-image" src="cafe/img/host2.jpg" alt="ホスト体験画像2">
+                <h3 class = "mini-box2-tittle" >コミュニティ</h3>
+
+            </div>
+            <div class = "mini-box3 ">
+                <img class = "mini-box3-image" src="cafe/img/host3.jpg" alt="ホスト体験画像3">
+                <h3 class = "mini-box3-tittle" >食べ歩き</h3>
+
+            </div>
+        </div>
+
+
+        </div>
+        
+    </div>
+
+    <footer class = "footer">
+    <div class = "wrap">
+        <div class = "footer-infos">
+            <div class = "footer-info">
+                <p class = "footer-info-tittle">企業情報</p>
+                <ul class = "footer-info-subject">
+                    <li>ご利用方法</li>
+                    <li>ニュースルーム</li>
+                    <li>株主・投資家のみなさまへ</li>
+                    <li>XXXXXXXXXXXXXXX</li>
+                    <li>XXXXXXXXXXXXXXX</li>
+                    <li>XXXXXXXXXXXXXXX</li>
+                    <li>採用情報</li>
+
+                    
+
+                </ul>
+                
+
+
+            </div>
+            <div class = "footer-info">
+                <p class = "footer-info-tittle">コミュニティ</p>
+                <ul class = "footer-info-subject">
+                    <li>ダイバーシティ</li>
+                    <li>アクセシビリティ対応</li>
+                    <li>お友達を招待</li>
+                    <li>XXXXXXXXXXXXXXX</li>
+                    <li>XXXXXXXXXXXXXXX</li>
+                </ul>
+                
+
+
+            </div>
+            <div class = "footer-info">
+                <p class = "footer-info-tittle">ホスト</p>
+                <ul class = "footer-info-subject">
+                    <li>XXXXXXXXXXXXXXX</li>
+                    <li>XXXXXXXXXXXXXXX</li>
+                    <li>XXXXXXXXXXXXXXX</li>
+                
+                </ul>
+                
+
+
+            </div>
+            <div class = "footer-info">
+                <p class = "footer-info-tittle">サポート</p>
+                <ul class = "footer-info-subject">
+                    <li>新型コロナウイルスに対する取り組み</li>
+                    <li>ヘルプセンター</li>
+                    <li>キャンセルオプション</li>
+                    <li>コミュニティサポート</li>
+                    <li>信頼＆安全</li>
+                </ul>
+                
+
+
+            </div>
+            
+        </div>
+        <div class ="copyright">
+            <ul class = "copyright-contents">
+                <li class = "copyright">このサイトの素材は全て著作権フリーのものを使用しています。</li>
+                <li class = "copyright">プライバシーポリシー 利用規約 サイトマップ 企業情報</li>
+                <li class = "copyright">© 2021- LiNew, Inc. All rights reserved.</li>
+            </ul>
+            
+        </div>
+    </div>
+
+    
+    
+
+
+    </footer>
+
+    
+
+
+   
+</div>
+
+<script>
+    $(function () {
+  // ヘッダーの位置
+  const pos = $('#js-header').offset().top;
+  // ヘッダーの高さ
+  const height = $('#js-header').outerHeight();
+
+  $(window).scroll(function () {
+    // スクロール量がヘッダーの位置を超えた場合
+    if ($(this).scrollTop() > pos) {
+      // ヘッダーにfixedクラスを付与
+      $('#js-header').addClass('fixed');
+      // bodyにヘッダーの高さ分のpadding-topをつける
+      $( 'body' ).css( 'padding-top', height);
+      //info-covidクラスをなくす
+      $( 'info-covid' ).css( 'display', none);
+
+
+
+      // 超えない場合
+      } else {
+      //ヘッダーのfixedクラスを外す
+      $('#js-header').removeClass('fixed');
+      // bodyのpadding-topをなくす
+      $( 'body' ).css( 'padding-top', 0);
+
+    }
+  });
+});
+
+</script>
+</body>
+
+
+</html>
